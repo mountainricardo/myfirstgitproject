ba11d504287e11cfbe49214ce1bb829c13cd6873
rserrano <rserrano@vectorsf.com>
2016-03-02 15:52:40 +0100

    primer commit de proyecto gulp0

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+gulp.task('default',function(){
+	// place for default task here
+	console.log('default task');
+});
+gulp.task('oink',function(){
+	console.log('oink');
+});
+gulp.task('oink2',function(){
+	console.log('oink2');
+} );
